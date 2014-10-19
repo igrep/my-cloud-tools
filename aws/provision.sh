e333d3eb369e5b4725b04e8befc8ef19696deefa
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-10-19 17:38:52 +0900

    add settings for localization: locale and timezone.

--- aws/provision.sh
+++ aws/provision.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+hwclock --systohc --utc
+
+cat <<END > /etc/locale.gen
+en_US.UTF-8 UTF-8
+ja_JP.UTF-8 UTF-8
+END
+locale-gen
+echo LANG=ja_JP.UTF-8 > /etc/locale.conf
+export LANG=ja_JP.UTF-8
+
 cat <<END > /etc/pacman.d/mirrorlist
 ## Score: 1.6, Japan
 Server = http://ftp.jaist.ac.jp/pub/Linux/ArchLinux/\$repo/os/\$arch
