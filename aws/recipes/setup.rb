7de51f7564b83f80a0711c5cdf48a2631a95e15a
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-16 12:09:25 +0900

    use unison instead of unison
    TODO: fix version inconsistency with remote server

--- aws/recipes/setup.rb
+++ aws/recipes/setup.rb
@@ -4,12 +4,8 @@
   'git',
   'tmux',
   'silversearcher-ag',
+  'unison',
   'make',
-  # GHC Dependencies
-  #'libgmp-dev',
-  #'gcc',
-  # end
-  'python2.7', # Dependency of dropbox-cli
 ].each do|package_name|
   package(package_name) { action :install }
 end
@@ -49,28 +45,4 @@ end
 
 file '/etc/sysctl.d/01-fs.inotify.max_user_watches.conf' do
   content "fs.inotify.max_user_watches=100000\n"
-end
-
-execute 'install dropbox daemon' do
-  not_if 'test -d .dropbox-dist/'
-  user 'admin'
-  command 'cd && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - '
-end
-
-directory('bin/'){ user 'admin' }
-
-execute 'install dropbox-cli' do
-  user 'admin'
-  not_if 'test -x bin/dropbox.py'
-  command '
-    cd bin && \
-    wget https://www.dropbox.com/download?dl=packages/dropbox.py -O dropbox.py && \
-    chmod 0755 dropbox.py
-  '
-end
-
-# This is just a memo
-execute 'configure dropbox by dropbox-cli' do
-  user 'admin'
-  command 'dropbox.py help'
 end
